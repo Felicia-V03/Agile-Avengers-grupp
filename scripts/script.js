2d2b6ea8197310bc60a53d56379dfc251d9cca9d
Felicia-V03 <3kdyhfe24.udomfe@folkuniversitetet.nu>
2025-03-11 12:48:44 +0100

    fix api och our-menu sidan

--- scripts/script.js
+++ scripts/script.js
@@ -2,29 +2,29 @@ import { getMenu } from "../modules/getMenu.js"
 import { openAndCloseNav } from "../modules/navMenu.js";
 
 //Till index sidan då visa den Trailers och Recommendations samt function för att söka
-if(window.location.pathname === '/' || window.location.pathname === '/Index.html') {
-    console.log('Index.html');
+if(window.location.pathname === '/' || window.location.pathname === '/index.html') {
+    console.log('index.html');
     time();
 
 //Till bår meny sidan för visa våran meny och hambergare meny som visa about us knappen
-} else if(window.location.pathname === '/Our_menu.html') {
-    console.log('Our_menu.html');
+} else if(window.location.pathname === '/our-menu.html') {
+    console.log('our-menu.htm');
     getMenu();
     openAndCloseNav();
 
-//Till favorites sidan då visa favoritfilmer samt function för att söka
-} else if(window.location.pathname === '/About_us.html') {
-    console.log('About_us.html');
+//Till about us sidan visa information om våran app
+} else if(window.location.pathname === '/about-us.html') {
+    console.log('about-us.html');
 
-//Till movie sidan då visa detaljer om filmen samt function för att söka
+//Till matlagningstid sidan visa upp lång tid det tar före användare kan hämta sin mat
 } else if(window.location.pathname === '/eta.html') {
     console.log('eta.html');
 
-//Till search sidan då visa sökresultat samt function för att söka
-} else if(window.location.pathname === '/my_order.html') {
-    console.log('my_order.html');
+//Till order sidan visa upp odersnummer och information
+} else if(window.location.pathname === '/my-order.html') {
+    console.log('my-order.html');
 
-//
+//Till kvittor sidan visa upp odersnummer och information
 } else if(window.location.pathname === '/receipt.html') {
     console.log('receipt.html');
 
@@ -32,6 +32,6 @@ if(window.location.pathname === '/' || window.location.pathname === '/Index.html
 
 function time() {
     setTimeout(function() {
-        window.location.href = "Our_menu.html";
+        window.location.href = "our-menu.html";
     }, 3000);
 }
