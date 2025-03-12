f8fd32e485753493a312668d7c1dda8398b331ae
Felicia-V03 <3kdyhfe24.udomfe@folkuniversitetet.nu>
2025-03-12 12:48:01 +0100

    fix order funktion

--- components/getcart.js
+++ components/getcart.js
@@ -1,12 +1,10 @@
 export function getCart() {
     document.addEventListener('DOMContentLoaded', async () => {
-      // Hämta favoritfilmer från localStorage
       const inCart = JSON.parse(localStorage.getItem('cart')) || [];
       
-      //Kontrollera om filmer finns i localStorage (om det inte finns några filmer då visa meddelande)
       if (inCart.length === 0) {
         const p = document.createElement('p');
-        p.textContent = 'No favorites added yet';
+        p.textContent = 'No items added yet';
         const orderContainer = getElement('.order-container');
         orderContainer.appendChild(p);
       } else {
@@ -22,31 +20,53 @@ function orderCard(menu) {
 }
 
 export function addToCart(menu, btn) {
-    const inCart = JSON.parse(localStorage.getItem('cart')) || [];
-  
     btn.addEventListener('click', () => {
   
-      const inCart = JSON.parse(localStorage.getItem('cart')) || [];
-  
-      if (btn) {
-        if (!inCart.some(cart => cart.Name === menu.Name)) {
-          inCart.push({
-            Name: menu.Name,
-            Ingredients: menu.Ingredients,
-            Description: menu.Description,
-            Price: menu.Price,
-            Type: menu.Type,
-          });
+        const inCart = JSON.parse(localStorage.getItem('cart')) || [];
+
+        const index = inCart.find(cart => cart.Name === menu.Name);
+
+        if (index) {
+            index.Antal += 1;
+        } else {
+            inCart.push({
+                Name: menu.Name,
+                Ingredients: menu.Ingredients,
+                Description: menu.Description,
+                Price: menu.Price,
+                Type: menu.Type,
+                Antal: 1
+            });
         }
-      } else {
-        const index = inCart.findIndex(cart => cart.Name === menu.Name);
-        if (index > -1) {
-          inCart.splice(index, 1);
-        }
-      }
-  
-      localStorage.setItem('cart', JSON.stringify(inCart));
-  
-      console.log('Cart:', inCart);
+      
+        localStorage.setItem('cart', JSON.stringify(inCart));
+      
+        console.log('Cart:', inCart);
+        const total = totalSum();
+        console.log('Total sum:', total);
+
     });
+}
+
+export function totalSum() {
+    const inCart = JSON.parse(localStorage.getItem('cart')) || [];
+
+    if (inCart.length > 0) {
+        let totalPrice = 0;
+
+        inCart.forEach(menu => {
+            const itemTotal = menu.Price * menu.Antal;
+
+            totalPrice += itemTotal;
+
+            console.log(`${menu.Name}: Total Price = ${itemTotal} (Price: ${menu.Price} * Antal: ${menu.Antal})`);
+        });
+
+        console.log('Total Price for all items:', totalPrice);
+
+        return totalPrice;
+    } else {
+        console.log('Varukorgen är tom');
+        return 0;
+    }
 }
